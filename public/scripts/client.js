701847f909e6f89541828a93e4775021892cab0f
Muna Dahir <muna.dahir@hotmail.com>
2021-11-17 23:00:39 -0500

    added tweets from ajax request, validated input, toggle form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,125 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+$(() => {
+ 
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  const createTweetElement = (tweetObj) => {
+    // create variable
+    const $tweet = `
+    <article  class="tweets">
+    <header>
+      <div>
+        <img src="${tweetObj.user.avatars}" > 
+        <!-- <i class="fab fa-earlybirds"></i> -->
+        <span>${tweetObj.user.name}</span>     
+      </div>
+      <div>
+        <span>${tweetObj.user.handle}</span>
+      </div>
+    </header>
+    
+        <div class="content">
+          <p>${escape(tweetObj.content.text)}</p>
+        </div>
+  
+     <footer>
+        <div>
+          <span>${timeago.format(tweetObj.created_at)}</span>
+        </div>
+        <div>
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+    </footer> 
+  </article>`
+    return $tweet;
+  }
+  
+  const renderTweets = function(tweets) {
+  
+    for (let tweet of tweets) {
+      const newTweet = createTweetElement(tweet)
+      $('.tweets-container').prepend(newTweet); 
+    }
+  }
+
+  
+   
+  $('#add-tweet').submit(function (event) {
+    event.preventDefault();
+  
+    let singleTweet = $('#tweet-text').val();
+    
+    let errMessage = "";
+    if (singleTweet.length > 140) {
+      errMessage = "You have entered too many characters.";
+      
+      return $(".err")
+      .text(errMessage)
+      .slideDown("fast", function () {
+      setTimeout(() => {
+        $(".err").slideUp("fast")
+      }, 2000 )
+      });
+
+      
+      
+      
+    } else if (singleTweet.length === 0) {
+     errMessage = " Your tweet does not have any content.";
+     
+     return $(".err")
+     .text(errMessage)
+     .slideDown("fast", function () {
+      setTimeout(() => {
+        $(".err").slideUp("fast")
+      }, 2000 )
+      });
+      
+    } 
+      
+
+    $.ajax({
+      method: 'post',
+      url: '/tweets',
+      data: $(this).serialize(),
+     })
+     .then(event)
+     .then(() => {
+      loadTweets();
+     })
+
+
+     this.reset();
+  
+  });
+  
+  const loadTweets = function() {
+    $.ajax('/tweets', {method: 'GET'}).then(function (data) {
+       renderTweets(data);
+     })
+
+     
+  };
+  loadTweets();
+  
+  
+
+  $(".nav-button").click(() => {
+    $(".new-tweet textarea").focus()
+    $(".new-tweet").slideDown();
+    });
+  
+  
+   
+  
+})
